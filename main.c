4b252b0089421226b51860130edbc121cda76228
wintermute <wintermute@paranoici.org>
2014-12-06 19:29:10 +0100

    center printed text

--- main.c
+++ main.c
@@ -2,28 +2,33 @@
 #include<ncurses.h>
 #include<stdlib.h>
 #include<signal.h>
+#include<string.h>
 
-void draw_menu(void);
-static void finish(int sig);
+//void draw_menu(void);
+//static void finish(int sig);
 
 int main(int argc, char **argv)
 {
+    int rows,cols;      // number of rows and columns of the current window
+    char msg[] = "FP address manager";
+
 	initscr();		// init the curses library
 	raw();			// line buffering disabled
 	keypad(stdscr, TRUE);	// enables function keys reading
 	noecho();
+    getmaxyx(stdscr, rows, cols);
 
-	mvprintw(0, 10, "FP address manager");
+	mvprintw(0, (cols-strlen(msg))/2 , msg);
     {
         int ch;
 
         ch = getch();
 
         if (ch == KEY_F(1)) {
-            printw("You pressed the F1 key");
+            mvprintw(2, (cols-strlen(msg))/2, "You pressed the F1 key");
         }
         else {
-            printw("You pressed ");
+            mvprintw(2, (cols-strlen(msg))/2, "You pressed ");
             attron(A_BOLD);
             printw("%c", ch);
             attroff(A_BOLD);
