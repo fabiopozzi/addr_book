dff74538b9cd3b5d75ed869cfa5d8e99eb4451b0
wintermute <wintermute@paranoici.org>
2014-12-06 19:21:56 +0100

    small improvements, curses window settings changed. print string, wait for input, print back the pressed key.

--- main.c
+++ main.c
@@ -9,8 +9,27 @@ static void finish(int sig);
 int main(int argc, char **argv)
 {
 	initscr();		// init the curses library
+	raw();			// line buffering disabled
+	keypad(stdscr, TRUE);	// enables function keys reading
+	noecho();
 
-	printw("FP address manager");
+	mvprintw(0, 10, "FP address manager");
+    {
+        int ch;
+
+        ch = getch();
+
+        if (ch == KEY_F(1)) {
+            printw("You pressed the F1 key");
+        }
+        else {
+            printw("You pressed ");
+            attron(A_BOLD);
+            printw("%c", ch);
+            attroff(A_BOLD);
+        }
+    }
+
 	refresh();
 	getch();
 	endwin();
