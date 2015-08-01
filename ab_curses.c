46c5e5e86ff2f855344b6b29a7b29d2f234f31f6
Fabio Pozzi <wintermute@antani.work>
2015-08-01 15:38:59 +0200

    code indent fixed to use tabs

--- ab_curses.c
+++ ab_curses.c
@@ -9,116 +9,116 @@
 void add_record(void);
 void curs_main(void)
 {
-    /* Init curses */
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
+	/* Init curses */
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
 }
 
 void print_choice(void)
 {
-    int ch;
+	int ch;
 
-    mvprintw(4, 10, "Welcome to the Address Book");
-    mvprintw(6, 10, "Press 'a' if you want to add a record");
-    mvprintw(8, 10, "Press 'p' to print the current address book");
-    mvprintw(10, 10, "Press 'm' to print the menu");
-    mvprintw(12, 10, "Press F1 to exit");
-    /* Loop through to get user requests */
-    /* F1 to exit */
-    while((ch = getch()) != KEY_F(1))
-    {
-        switch(ch)
-        {
-        case 'a':
-            clear();
-            refresh();
-            mvprintw(4, 10, "Add a new record");
-            add_record();
-            break;
-        case 'p':
-            clear();
-            refresh();
-            mvprintw(4, 10, "Print all records");
-            break;
-        case 'm':
-            clear();
-            refresh();
-            mvprintw(4, 10, "Welcome to the Address Book");
-            mvprintw(6, 10, "Press 'a' if you want to add a record");
-            mvprintw(8, 10, "Press 'p' to print the current address book");
-            mvprintw(10, 10, "Press 'm' to print the menu");
-            mvprintw(12, 10, "Press F1 to exit");
-        default:
-            /* if it's a normal char, it gets printed */
-            /*form_driver(my_form, ch);*/
-            break;
-        }
-    }
+	mvprintw(4, 10, "Welcome to the Address Book");
+	mvprintw(6, 10, "Press 'a' if you want to add a record");
+	mvprintw(8, 10, "Press 'p' to print the current address book");
+	mvprintw(10, 10, "Press 'm' to print the menu");
+	mvprintw(12, 10, "Press F1 to exit");
+	/* Loop through to get user requests */
+	/* F1 to exit */
+	while((ch = getch()) != KEY_F(1))
+	{
+		switch(ch)
+		{
+		case 'a':
+			clear();
+			refresh();
+			mvprintw(4, 10, "Add a new record");
+			add_record();
+			break;
+		case 'p':
+			clear();
+			refresh();
+			mvprintw(4, 10, "Print all records");
+			break;
+		case 'm':
+			clear();
+			refresh();
+			mvprintw(4, 10, "Welcome to the Address Book");
+			mvprintw(6, 10, "Press 'a' if you want to add a record");
+			mvprintw(8, 10, "Press 'p' to print the current address book");
+			mvprintw(10, 10, "Press 'm' to print the menu");
+			mvprintw(12, 10, "Press F1 to exit");
+		default:
+			/* if it's a normal char, it gets printed */
+			/*form_driver(my_form, ch);*/
+			break;
+		}
+	}
 }
 
 void curs_exit(void)
 {
-    endwin();
+	endwin();
 }
 
 void add_record(void)
 {
-    FIELD *field[4];
-    FORM    *my_form;
-    int ch;
-    /* Init fields */
-    field[0] = new_field(1, 10, 6, 18, 0, 0);
-    field[1] = new_field(1, 10, 8, 18, 0, 0);
-    field[2] = NULL;
+	FIELD *field[4];
+	FORM    *my_form;
+	int ch;
+	/* Init fields */
+	field[0] = new_field(1, 10, 6, 18, 0, 0);
+	field[1] = new_field(1, 10, 8, 18, 0, 0);
+	field[2] = NULL;
 
-    set_field_back(field[0], A_UNDERLINE);
-    field_opts_off(field[0], O_AUTOSKIP);
+	set_field_back(field[0], A_UNDERLINE);
+	field_opts_off(field[0], O_AUTOSKIP);
 
-    set_field_back(field[1], A_UNDERLINE);
-    field_opts_off(field[1], O_AUTOSKIP);
+	set_field_back(field[1], A_UNDERLINE);
+	field_opts_off(field[1], O_AUTOSKIP);
 
-    /* Create the form and post it */
-    my_form = new_form(field);
-    post_form(my_form);
-    refresh();
+	/* Create the form and post it */
+	my_form = new_form(field);
+	post_form(my_form);
+	refresh();
 
-    mvprintw(6, 10, "Nome: ");
-    mvprintw(8, 10, "Cognome: ");
-    refresh();
+	mvprintw(6, 10, "Nome: ");
+	mvprintw(8, 10, "Cognome: ");
+	refresh();
 
-    /* Loop through to get user requests */
-    /* F1 to exit */
-    while((ch = getch()) != KEY_F(10))
-    {
-        switch(ch)
-        {
-        case KEY_DOWN:
-            /* Go to next field */
-            form_driver(my_form, REQ_NEXT_FIELD);
-            /* Go to the end of the present buffer */
-            /* Leaves nicely at the last character */
-            form_driver(my_form, REQ_END_LINE);
-            break;
-        case KEY_UP:
-            /* go to previous field */
-            form_driver(my_form, REQ_PREV_FIELD);
-            form_driver(my_form, REQ_END_LINE);
-            break;
-        case KEY_F(10):
-            mvprintw(10, 10, "Saving...");
-        default:
-            /* if it's a normal char, it gets printed */
-            form_driver(my_form, ch);
-            break;
-        }
-    }
+	/* Loop through to get user requests */
+	/* F1 to exit */
+	while((ch = getch()) != KEY_F(10))
+	{
+		switch(ch)
+		{
+		case KEY_DOWN:
+			/* Go to next field */
+			form_driver(my_form, REQ_NEXT_FIELD);
+			/* Go to the end of the present buffer */
+			/* Leaves nicely at the last character */
+			form_driver(my_form, REQ_END_LINE);
+			break;
+		case KEY_UP:
+			/* go to previous field */
+			form_driver(my_form, REQ_PREV_FIELD);
+			form_driver(my_form, REQ_END_LINE);
+			break;
+		case KEY_F(10):
+			mvprintw(10, 10, "Saving...");
+		default:
+			/* if it's a normal char, it gets printed */
+			form_driver(my_form, ch);
+			break;
+		}
+	}
 
-    /* Un-post form and free memory */
-    unpost_form(my_form);
-    free_form(my_form);
-    free_field(field[0]);
-    free_field(field[1]);
+	/* Un-post form and free memory */
+	unpost_form(my_form);
+	free_form(my_form);
+	free_field(field[0]);
+	free_field(field[1]);
 }
 
--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 
 struct list_t
 {
-    int idx;
-    char *nome;
-    char *cognome;
-    struct list_t *next;
+	int idx;
+	char *nome;
+	char *cognome;
+	struct list_t *next;
 };
 
 struct list_t *b1 = NULL;
@@ -18,90 +18,90 @@ int i=0;
 
 int add_to_book(struct list_t **book)
 {
-    struct list_t *tmp;
-    char line[30];
+	struct list_t *tmp;
+	char line[30];
 
-    tmp = (struct list_t *)malloc(sizeof(struct list_t));
-    if (tmp == NULL) {
-        fprintf(stderr, "[error] Malloc failed\n");
-        return -1;                         // TODO: usare valori di ritorno codificati ?
-    }
+	tmp = (struct list_t *)malloc(sizeof(struct list_t));
+	if (tmp == NULL) {
+		fprintf(stderr, "[error] Malloc failed\n");
+		return -1;	// TODO: usare valori di ritorno codificati ?
+	}
 
-    // inserimento in testa
-    tmp->next = *book;
-    tmp->idx = i;
-    tmp->nome = (char *)malloc(STR_MAX_LEN * sizeof(char));
-    if(tmp->nome == NULL) {
-        fprintf(stderr, "[error] Malloc failed\n");
-        return -1;
-    }
-    printf("Inserisci nome, (max 20 caratteri)\n");
-    if (fgets(line, sizeof(line), stdin) != NULL) {
-        sscanf(line,"%[^\n]", tmp->nome);
-    }
-    else {
-        return -1;
-    }
-    *book = tmp;
-    printf("aggiunto elemento %d\n",i);
-    i++;
+	// inserimento in testa
+	tmp->next = *book;
+	tmp->idx = i;
+	tmp->nome = (char *)malloc(STR_MAX_LEN * sizeof(char));
+	if(tmp->nome == NULL) {
+		fprintf(stderr, "[error] Malloc failed\n");
+		return -1;
+	}
+	printf("Inserisci nome, (max 20 caratteri)\n");
+	if (fgets(line, sizeof(line), stdin) != NULL) {
+		sscanf(line,"%[^\n]", tmp->nome);
+	}
+	else {
+		return -1;
+	}
+	*book = tmp;
+	printf("aggiunto elemento %d\n",i);
+	i++;
 
-    return 0;
+	return 0;
 }
 
 int print_book(struct list_t *book)
 {
-    struct list_t *lp, *oldlp;
+	struct list_t *lp, *oldlp;
 
-    for(lp = book; lp != NULL; oldlp = lp, lp = lp->next, free(oldlp))
-    {
-        printf("elemento lista numero %d\n", lp->idx);
-        if (lp->nome != NULL) {
-            printf("Nome: %s\n",lp->nome);
-            free(lp->nome);
-        }
-    }
-    return 0;
+	for(lp = book; lp != NULL; oldlp = lp, lp = lp->next, free(oldlp))
+	{
+		printf("elemento lista numero %d\n", lp->idx);
+		if (lp->nome != NULL) {
+			printf("Nome: %s\n",lp->nome);
+			free(lp->nome);
+		}
+	}
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    /*char c,foo;*/
-    /*int go = 1;*/
-    /*int ret;*/
+	/*char c,foo;*/
+	/*int go = 1;*/
+	/*int ret;*/
 
-    curs_main();
-    print_choice();
-    curs_exit();
-    // TODO: usare getopt?
- #if 0
-    printf("cosa fare?\n");
-    printf("a per aggiungere un record\n");
-    printf("e per uscire\n");
-    while (go) {
-        printf("> ");
-        scanf("%c%c", &c, &foo);
-        switch (c)
-        {
-            case 'a':
-                ret = add_to_book(&b1);
-                if(ret < 0)
-                    return -1;
-                printf("ora b1 vale %p\n", b1);
-                break;
-            case 'e':
-                go = 0;
-                break;
-            default:
-                printf("opzione %c non riconosciuta\n", c);
-                break;
-        }
-    }
-    printf("hai deciso di uscire, stampo il libro attuale\n");
-    print_book(b1);
+	curs_main();
+	print_choice();
+	curs_exit();
+	// TODO: usare getopt?
+#if 0
+	printf("cosa fare?\n");
+	printf("a per aggiungere un record\n");
+	printf("e per uscire\n");
+	while (go) {
+		printf("> ");
+		scanf("%c%c", &c, &foo);
+		switch (c)
+		{
+		case 'a':
+			ret = add_to_book(&b1);
+			if(ret < 0)
+				return -1;
+			printf("ora b1 vale %p\n", b1);
+			break;
+		case 'e':
+			go = 0;
+			break;
+		default:
+			printf("opzione %c non riconosciuta\n", c);
+			break;
+		}
+	}
+	printf("hai deciso di uscire, stampo il libro attuale\n");
+	print_book(b1);
 #endif
 
-    return 0;
+	return 0;
 }
 
 
